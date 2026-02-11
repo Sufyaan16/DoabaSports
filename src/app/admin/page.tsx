ba6d87daed147952d91d07cd0852aa7ce4659c79
Sufyaan-16 <swen231102016@kfueit.edu.pk>
2026-02-11 16:05:50 +0500

    no changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -30,7 +30,7 @@ export default async function AdminDashboard() {
   return (
     <>
       <SiteHeader />
-      <div className="flex flex-1 flex-col gap-8 p-4 lg:p-6">
+      <div className="flex flex-1 flex-col gap-8 p-4 lg:p-6 ">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <SectionCards stats={stats} />
